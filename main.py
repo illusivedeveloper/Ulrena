f704786362cee818006d086e3bc886bbe25b9115
illusivedeveloper <illusivedeveloper@gmail.com>
2020-04-17 23:16:24 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, escape, request, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/error")
+def error():
+    return render_template("error.html")
