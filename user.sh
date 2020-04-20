6b17ec9342bb787187d1adcc8078a23db280a77a
illusivedeveloper <illusivedeveloper@gmail.com>
2020-04-20 10:28:39 +0530

    java update

--- user.sh
+++ user.sh
@@ -10,4 +10,3 @@ sudo runuser -l ansible -c "chmod 600 /home/ansible/.ssh/authorized_keys"
 sudo runuser -l ansible -c "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpCm5MdGExF/1CzUNsbw4RBai7kMpmR7JhYZtbAGGShYRR/NiLH2GJjaDpQ2mO6JbQBvyfxs7dW+cJNBK4uhxQUuIg2wvnaLsz5EUVYwSXOJbHLqUepFjIwu7EeAFYOZiHvr3ZNNa5eqE5JQWdyiDrk4oBdmFzACmTiKaDhd5FDAmYO0u+nb9we+EQ94ZcT96KDk1tlUmi919rrNiiV328e5mYn6cB4FbyqYRAYgGgBXsmRP2oRlaHltBC9Mb47mYIf1YileeGLm72D148BOGXU/aeG0oIXrStqrhiXF5+j9wCgWqoovXzAX+mRZYwM06wU1xZLDyO18mZgZWIu5bJ useast2' > /home/ansible/.ssh/authorized_keys"
 echo "ansible ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ansible
 sudo chmod 0440 /etc/sudoers.d/ansible
-sudo yum install java-1.8.0-openjdk-devel -y
